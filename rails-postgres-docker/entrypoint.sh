a0da55c5d844374c72b94d08345d98a43e5307d0
Ramakrishnaragi <152708470+Ramakrishnaragi@users.noreply.github.com>
2025-05-02 15:20:46 +0530

    Update entrypoint.sh

--- rails-postgres-docker/entrypoint.sh
+++ rails-postgres-docker/entrypoint.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 set -e
 
-# Wait for PostgreSQL
-until pg_isready -h db -p 5432 -U postgres; do
+# Wait for PostgreSQL to be ready
+until pg_isready -h $DATABASE_HOST -p 5432; do
   echo "Waiting for PostgreSQL..."
   sleep 2
 done
 
-exec "$@"
+# Run the migrations or prepare the database (optional)
+bundle exec rake db:prepare
 
+# Remove a potentially pre-existing server.pid
+rm -f /myapp/tmp/pids/server.pid
+
+exec "$@"
